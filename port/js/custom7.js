38e7d60913220c809d335c9257ef75858478ba99
ay9318 <ay9318@naver.com>
2020-07-20 13:02:02 +0900

    2020.07.20

--- port/js/custom7.js
+++ port/js/custom7.js
@@ -1,3 +1,56 @@
+function imagesProgress(){
+    var $container = $("#progress"),
+        $progressText = $container.find(".progress-text"),
+        imgLoad = imagesLoaded("body"),	
+        imgTotal = imgLoad.images.length,	
+        imgLoaded = 0,										
+        current = 0,							
+        progressTimer = setInterval(updateProgress, 2000 / 60);	
+
+        imgLoad.on("progress", function(){
+            imgLoaded++;
+        });
+
+    function updateProgress(){
+        var target = ( imgLoaded / imgTotal) * 100;
+
+        current += ( target - current) * 0.1;
+        $progressText.text( Math.floor(current) + '%' );
+
+        if(current >= 100){
+            clearInterval(progressTimer);
+            $container.animate({opacity: '0'},1500,'easeInOutQuint').animate({top: '-100%'},1500);
+            
+            gsap.to(".main .main-left h2 span", {opacity: 1, y: 0, duration: 2.5, delay: 2, stagger: 0.1, ease: "elastic.out(1, 0.3)"});
+            gsap.to(".main .main-left p span", {opacity: 1, duration: 0.2, delay: 3.5, stagger: 0.06});
+            gsap.to(".img-wrap .heart", {opacity: 1, duration: 0.5, delay: 3});
+            gsap.to(".img-wrap .monkey", {opacity: 1, duration: 0.5, delay: 3.4});
+            gsap.to(".img-wrap .duck", {opacity: 1, duration: 0.5, delay: 3.8});
+            gsap.to("#header", {top: 0, opacity: 1, duration: 0.7, delay: 4});
+            gsap.to(".gift", {left: 0, duration: 0.7, delay: 4});
+            
+            
+            
+            
+            
+        }
+        if(current > 99.9){
+            current = 100;
+        };
+    };
+};
+
+imagesProgress();
+
+$(".splice").each(function () {
+    let txt = $(this).text();
+    let split = txt.split("").join("</span><span aria-hidden='true'>");
+    split = "<span aria-hidden='true'>" + split + "</span>";
+    $(this).html(split).attr("aria-label", txt);
+});
+
+
+
 //커서
 $(".cursor img").hide();
 
